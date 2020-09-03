8888fe34e0ecace94f13e4db4c562c10f5349c68
pipesonido <pipesonido@gmail.com>
2020-09-02 21:14:47 -0500

    Agregar carpeta Ejemplo1

---
+++ Ejemplo1/variables.js
@@ -0,0 +1,12 @@
+//Scope
+let nombre = 'Fernando'
+let edad = 35
+estatura = 1.89
+const fechaEspecial = "01-09-87"
+const estado_casado = true
+//const
+//var
+
+//'Tu nomnbre es 
+
+document.write(`Tu nombre es ${nombre} y tu edad es ${edad} años y mas`)
